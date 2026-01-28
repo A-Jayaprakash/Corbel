57c3764bec823aa925a124f881e67a5eb585c92d
A-Jayaprakash <jayaprakashoffic@gmail.com>
2026-01-28 21:18:29 +0530

    feat(auth): add bcrypt password hashing utilities

---
+++ src/utils/password.util.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 12;
+
+/**
+ *
+ * @param {string} password
+ * @returns {Promise<string>} hashed password
+ */
+
+export const hashPassword = async (oassword) => {
+  if (!password) {
+    throw new Error("Password is required");
+  }
+  return bcrypt.hash(password, SALT_ROUNDS);
+};
+
+/**
+ *
+ * @param {string} password
+ * @param {string} hashpassword
+ * @returns {Promise<boolean>}
+ */
+
+export const comparePassword = async (password, hashpassword) => {
+  if (!password || !hashpassword) {
+    throw new Error(
+      "Both password and hashpassword are required for comparison",
+    );
+  }
+  return bcrypt.compare(password, hashpassword);
+};
